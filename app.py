8849d032061a4738ae276727643edce7751bfa71
jorginnifb <jorge.edu.paulo2020@gmail.com>
2026-08-27 12:39:51 +0000

    cr7

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import from flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def ola_mundo():
-    return render_template("index.html")
+    return render_template("login.html")
 
-if __name__ == " __main__":
-    app.run(debug=True)Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def ola_mundo():
-    return render_template("index.html")
+@app.route("/painel")
+def painel():
+    return render_template("painel.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
